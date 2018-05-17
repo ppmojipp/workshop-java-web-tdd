d37aa4552457cc8955f6ce9dd4e5b858a1a09552
Nattapong Payuckkaso <Nattapong.P@sysprod.local>
2018-05-17 14:07:58 +0700

    Upload

--- src/test/java/dao/UserDaoTest.java
+++ src/test/java/dao/UserDaoTest.java
@@ -27,7 +27,8 @@ public class UserDaoTest {
 	private static final String JDBC_DRIVER = org.h2.Driver.class.getName();
     private static final String JDBC_URL = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
     private static final String USER = "sa";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "xx";
+    //private static final String PASSWORD = "";
     private static final String UTF8 = "UTF8";
 
     @BeforeClass
